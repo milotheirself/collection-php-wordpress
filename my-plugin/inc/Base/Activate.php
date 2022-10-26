5505210c7c2c8b9f4df097874078e59acae79067
Abbey <54866561+milotheirself@users.noreply.github.com>
2022-10-26 11:46:21 +0200

    Part 16

---
+++ my-plugin/inc/Base/Activate.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @package  MyPlugin
+ */
+namespace Inc\Base;
+
+class Activate
+{
+    public static function activate()
+    {
+        flush_rewrite_rules();
+    }
+}
---
+++ my-plugin/inc/Base/Deactivate.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @package  MyPlugin
+ */
+namespace Inc\Base;
+
+class Deactivate
+{
+    public static function deactivate()
+    {
+        flush_rewrite_rules();
+    }
+}
