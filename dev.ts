4316ef39ddad425aac7203f46fd216de0f67e38b
Abbey <54866561+milotheirself@users.noreply.github.com>
2022-10-28 11:23:45 +0200

    refactor: split file observer

--- dev.ts
+++ dev.ts
@@ -3,7 +3,17 @@ import appPaths from 'https://github.com/wesauis/deno-app-paths/raw/0.1.1/mod.ts
 import { resolve } from 'https://deno.land/std@0.155.0/node/path.ts';
 import { copy } from 'https://deno.land/std@0.155.0/fs/mod.ts';
 
-const tmp = resolve('./my-plugin');
-const out = resolve('C:/Application/xampp/htdocs/wp-content/plugins', './my-plugin');
+await Promise.all([
+  ...['my-plugin'].map(async (pat) => {
+    const tmp = resolve(`./${pat}`);
+    const out = resolve(`C:/Application/xampp/htdocs/wp-content/plugins/${pat}`);
 
-await copy(tmp, out, { overwrite: true });
+    await copy(tmp, out, { overwrite: true });
+  }),
+  ...['my-themes'].map(async (pat) => {
+    const tmp = resolve(`./${pat}`);
+    const out = resolve(`C:/Application/xampp/htdocs/wp-content/themes/${pat}`);
+
+    await copy(tmp, out, { overwrite: true });
+  }),
+]);
